a23fbb81c4513a19179bc581e15186e76efe7918
Matias Risco <matiasriscolagos@gmail.com>
2023-11-27 21:55:01 +0100

    Add cors configuration

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,7 @@
+import cors from "cors";
 import pingRouter from "../feature/ping/router/pingRouter.js";
 import app from "./app.js";
+import { corsOptions } from "./utils/corsOptions.js";
 
+app.use(cors(corsOptions));
 app.get("/", pingRouter);
